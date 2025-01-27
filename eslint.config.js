f3db4158e569aaea1ee843c637251fde023f7cdf
devshanematte <devshanematte@gmail.com>
2025-01-27 10:06:40 +0500

    update

--- eslint.config.js
+++ eslint.config.js
@@ -19,6 +19,8 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-implicit-any': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
